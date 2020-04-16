86ac1127d0521baf86120b0a1c77a272fbcee6ea
Froy <fdaf58@gmail.com>
2020-04-16 00:00:50 -0500

    patter strategy

--- java-design-patterns/src/main/java/designpatterns/behavioral/strategy/AppDesignPatternStrategy1.java
+++ java-design-patterns/src/main/java/designpatterns/behavioral/strategy/AppDesignPatternStrategy1.java
@@ -6,19 +6,27 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.reflect.MethodUtils;
 import org.apache.commons.text.WordUtils;
 
+import java.lang.reflect.Method;
+
 
 public class AppDesignPatternStrategy1 {
 
     public static void main(String[] args) throws Exception {
+
+        VendedorNivel vendedorNivel = new VendedorNivel(new VendedorNivel1());
+        System.out.println(vendedorNivel.profitFromSale(50));
+
         String tipoDeVendedorFromDatabase = "VENDEDOR_NIVEL_1";
         String tipoDeVendedorParsed = parseTipoDeVendedorLikeNameClass(tipoDeVendedorFromDatabase);
-        VendedorNivel vendedorNivel = new VendedorNivel(new VendedorNivel1());
+        final Class<?> clazz = ClassUtils.getClass("designpatterns.behavioral.strategy." + tipoDeVendedorParsed);
+//        Object profitFromSale = MethodUtils.invokeStaticMethod(clazz, "profitFromSale", 50);
 
+        Object vendedorNivelTest = clazz.getDeclaredConstructor().newInstance();
+        Object profitFromSaleInvoke = MethodUtils.invokeMethod(vendedorNivelTest, "profitFromSale", 50);
 
-        final Class<?> clazz = ClassUtils.getClass("designpatterns.behavioral.strategy."+tipoDeVendedorParsed);
-        MethodUtils.getAccessibleMethod(clazz,"profitFromSale");
-        Object profitFromSale = MethodUtils.invokeStaticMethod(clazz, "profitFromSale", 50);
-        System.out.println(profitFromSale);
+        System.out.println(profitFromSaleInvoke);
+        VendedorNivel vendedorNivelTest2 = new VendedorNivel( (StrategyVendedor) vendedorNivelTest);
+        System.out.println(vendedorNivelTest2.profitFromSale(50));
 
     }
 
