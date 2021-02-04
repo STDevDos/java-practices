a58f3f3058dd37cfe2238891d873981111543049
Froy <fdaf58@gmail.com>
2021-02-04 01:07:23 -0600

    java 15

--- java-ejercicios/src/main/java/quick/OptionalTest.java
+++ java-ejercicios/src/main/java/quick/OptionalTest.java
@@ -1,13 +1,61 @@
 package quick;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.util.Optional;
 
 public class OptionalTest {
 
     public static void main(String[] args) {
+        quickTest2();
+    }
 
+    @Data
+    static class Player {
+        private Short status;
+    }
+
+    @AllArgsConstructor
+    static class House {
+        Optional<Room> room;
+
+        public Optional<Room> getRoom() {
+            return room;
+        }
+    }
+
+    @AllArgsConstructor
+    static class Room {
+        Optional<Table> table;
+
+        public Optional<Table> getTable() {
+            return table;
+        }
+    }
+
+    @AllArgsConstructor
+    static class Table {
+        String colour;
+
+        public String getColour() {
+            return colour;
+        }
+    }
+
+    private static void quickTest2() {
+        //Player player = null;
+        Player player = new Player();
+        player.setStatus(new Short("6"));
+
+        Short statusCode = Optional.ofNullable(player)
+                .map(Player::getStatus).orElse(new Short("3"));
+
+        System.out.println(statusCode);
+
+    }
+
+    private static void quickTest1() {
         //Table table1 = new Table("black");
 //        Table table1 = null;
 //        Boolean isBlueTable = Optional.ofNullable(table1)
@@ -31,33 +79,6 @@ public class OptionalTest {
                 .orElse("black");
 
         System.out.println(tableColour);
-
-
-    }
-
-    @AllArgsConstructor
-    static class House {
-        Optional<Room> room;
-        public Optional<Room> getRoom() {
-            return room;
-        }
-    }
-
-    @AllArgsConstructor
-    static class Room {
-        Optional<Table> table;
-        public Optional<Table> getTable() {
-            return table;
-        }
-    }
-
-    @AllArgsConstructor
-    static class Table {
-        String colour;
-
-        public String getColour() {
-            return colour;
-        }
     }
 
 }
---
+++ java-java15/src/main/java/me/froy/main/StringChanges.java
@@ -0,0 +1,22 @@
+package me.froy.main;
+
+public class StringChanges {
+
+    record UserRecord(int userId, String userName) {
+    }
+
+    public static void main(String[] args) {
+
+        UserRecord userRecord = new UserRecord(1111, "JavaBeam");
+        System.out.println(userRecord.userName());
+
+        var textBlock = """
+                Name | Address | City
+                Bob Smith | 123 Anytown St\nApt 100 | Vancouver
+                Jon Brown | 1000 Golden Place\nSuite 5 | Santa Ana
+                """;
+        System.out.println(textBlock);
+
+    }
+
+}
